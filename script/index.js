93f32ba5dfe424b354ea465ae1853f5d9f5b773e
Fady Samir Sadek <fadysamirsadek@gmail.com>
2021-03-28 18:51:00 +0200

    homepage

--- script/index.js
+++ script/index.js
@@ -1,22 +1,17 @@
 (function () {
-  const workItems = document.getElementsByClassName("work-item");
-  Object.entries(workItems).map((workItem) => {
-    /*preload images */
-    var img = new Image();
-    img.src = `url('./assets/images/homepage/${workItem[1].id}.png')`;
-    /*End preload images */
-    document
-      .getElementById(workItem[1].id)
-      .addEventListener("mouseover", () => {
-        document.getElementById(
-          "container"
-        ).style.background = `url('./assets/images/homepage/${workItem[1].id}.png') no-repeat center center fixed`;
-        document.getElementById("container").style.backgroundSize = "cover";
-      });
-    document
-      .getElementById(workItem[1].id)
-      .addEventListener("mouseleave", () => {
-        document.getElementById("container").style.background = "white";
-      });
+  const workContainer = document.getElementById("work");
+  const repeatedElements = workContainer.innerHTML.repeat(100);
+  document.getElementById("work").innerHTML = repeatedElements;
+  document.querySelectorAll(".work-item").forEach((item) => {
+    item.addEventListener("mouseover", (event) => {
+      const imageName = event.target.getAttribute("data-image");
+      document.getElementById(
+        "container"
+      ).style.background = `url('./assets/images/homepage/${imageName}.png') no-repeat center center fixed`;
+      document.getElementById("container").style.backgroundSize = "cover";
+    });
+    item.addEventListener("mouseout", () => {
+      document.getElementById("container").style.background = "white";
+    });
   });
 })();
